9fec99ef0eccb7665518cc39c84abd99eb204bb0
rahulsnair <rahulsnair.vdp@gmail.com>
2026-02-01 02:18:48 +0530

    include external/bazelbuild-platforms

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 KERNEL_ROOT_DIR=$PWD
 TARGET_PRODUCT=cybert
 KERNEL_DEFCONFIG="mgk_64_k61_defconfig"
@@ -9,15 +10,29 @@ KERNEL_DEFCONFIG_OVERLAYS="mgk_64_k61_defconfig"
 KERNEL_BAZEL_BUILD_OUT=out/target/product/${TARGET_PRODUCT}/obj/KLEAF_OBJ
 KERNEL_BAZEL_DIST_OUT=out/target/product/${TARGET_PRODUCT}/obj/KLEAF_OBJ/dist
 
+# Link Bazel files
+if [ ! -f "MODULE.bazel" ]; then
+    echo "Creating MODULE.bazel symlink..."
+    ln -s build/kernel/kleaf/bzlmod/bazel.MODULE.bazel MODULE.bazel
+fi
+if [ ! -f "WORKSPACE.bzlmod" ]; then
+    echo "Creating WORKSPACE.bzlmod symlink..."
+    ln -s build/kernel/kleaf/bzlmod/bazel.WORKSPACE.bzlmod WORKSPACE.bzlmod
+fi
+if [ ! -L "WORKSPACE" ] && [ ! -f "WORKSPACE" ]; then
+    echo "Creating empty WORKSPACE file..."
+    touch WORKSPACE
+fi
+
 export BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1 DEFCONFIG_OVERLAYS="mt6897_overlay.config cybert_overlay.config" KERNEL_VERSION=kernel-6.1 SOURCE_DATE_EPOCH=0 JAVA_HOME="${KERNEL_ROOT_DIR}/prebuilts/jdk/jdk11/linux-x86" PATH="${KERNEL_ROOT_DIR}/prebuilts/jdk/jdk11/linux-x86/bin:${PATH}"
 
-PRIVATE_BAZEL_BUILD_FLAG="--//build/bazel_mgk_rules:kernel_version=${LINUX_KERNEL_VERSION#kernel-} --experimental_writable_outputs --noenable_bzlmod --config=stamp --repo_manifest=${KERNEL_ROOT_DIR}/${KERNEL_DIR}/fake_manifest.xml"
+PRIVATE_BAZEL_BUILD_FLAG="--experimental_writable_outputs --config=stamp --repo_manifest=${KERNEL_ROOT_DIR}/${KERNEL_DIR}/fake_manifest.xml"
 
 my_kernel_target=${KERNEL_DEFCONFIG%_defconfig}
 
-PRIVATE_BAZEL_BUILD_GOAL="//${KERNEL_DIR}:${my_kernel_target}_customer_modules_install.${KERNEL_BUILD_VARIANT}"
+PRIVATE_BAZEL_BUILD_GOAL="//${KERNEL_DIR}:mgk_64_k61_modules_install"
 
-PRIVATE_BAZEL_DIST_GOAL="//${KERNEL_DIR}:${my_kernel_target}_customer_dist.${KERNEL_BUILD_VARIANT}"
+PRIVATE_BAZEL_DIST_GOAL="//${KERNEL_DIR}:mgk_64_k61_dist"
 
 build/kernel/kleaf/bazel.sh --output_root=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT} --output_base=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT}/bazel/output_user_root/output_base build ${PRIVATE_BAZEL_BUILD_FLAG} ${PRIVATE_BAZEL_BUILD_GOAL}
 
