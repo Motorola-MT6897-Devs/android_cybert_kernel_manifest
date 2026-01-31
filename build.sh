f9cbde82453b99f5c213b0752d07374a7d107cfc
rahulsnair <rahulsnair.vdp@gmail.com>
2026-01-31 18:14:47 +0530

    add build script

---
+++ build.sh
@@ -0,0 +1,34 @@
+KERNEL_ROOT_DIR=$PWD/kernel_platform
+TARGET_PRODUCT=cybert
+KERNEL_DEFCONFIG="mgk_64_k61_defconfig"
+KERNEL_BUILD_VARIANT=user
+KERNEL_TARGET_ARCH=arm64
+KERNEL_DIR="kernel_device_modules-6.1"
+LINUX_KERNEL_VERSION="kernel-6.1"
+KERNEL_DEFCONFIG_OVERLAYS="mgk_64_k61_defconfig"
+KERNEL_BAZEL_BUILD_OUT=out/target/product/${TARGET_PRODUCT}/obj/KLEAF_OBJ
+KERNEL_BAZEL_DIST_OUT=out/target/product/${TARGET_PRODUCT}/obj/KLEAF_OBJ/dist
+
+cd kernel_platform
+
+# Link WORKSPACE if missing
+if [ ! -f "WORKSPACE" ]; then
+    echo "Creating WORKSPACE symlink..."
+    ln -s build/kernel/kleaf/bazel.WORKSPACE WORKSPACE
+fi
+
+export BAZEL_DO_NOT_DETECT_CPP_TOOLCHAIN=1 DEFCONFIG_OVERLAYS="mt6897_overlay.config cybert_overlay.config" KERNEL_VERSION=kernel-6.1 SOURCE_DATE_EPOCH=0 JAVA_HOME="${KERNEL_ROOT_DIR}/prebuilts/jdk/jdk11/linux-x86" PATH="${KERNEL_ROOT_DIR}/prebuilts/jdk/jdk11/linux-x86/bin:${PATH}"
+
+PRIVATE_BAZEL_BUILD_FLAG="--//build/bazel_mgk_rules:kernel_version=${LINUX_KERNEL_VERSION#kernel-} --experimental_writable_outputs --noenable_bzlmod --config=stamp --repo_manifest=${KERNEL_ROOT_DIR}/${KERNEL_DIR}/fake_manifest.xml"
+
+my_kernel_target=${KERNEL_DEFCONFIG%_defconfig}
+
+PRIVATE_BAZEL_BUILD_GOAL="//${KERNEL_DIR}:${my_kernel_target}_customer_modules_install.${KERNEL_BUILD_VARIANT}"
+
+PRIVATE_BAZEL_DIST_GOAL="//${KERNEL_DIR}:${my_kernel_target}_customer_dist.${KERNEL_BUILD_VARIANT}"
+
+build/kernel/kleaf/bazel.sh --output_root=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT} --output_base=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT}/bazel/output_user_root/output_base build ${PRIVATE_BAZEL_BUILD_FLAG} ${PRIVATE_BAZEL_BUILD_GOAL}
+
+build/kernel/kleaf/bazel.sh --output_root=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT} --output_base=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT}/bazel/output_user_root/output_base run ${PRIVATE_BAZEL_BUILD_FLAG} --nokmi_symbol_list_violations_check ${PRIVATE_BAZEL_DIST_GOAL} -- --dist_dir=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_DIST_OUT}
+
+build/kernel/kleaf/bazel.sh --output_root=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT} --output_base=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_BUILD_OUT}/bazel/output_user_root/output_base run ${PRIVATE_BAZEL_BUILD_FLAG} //${LINUX_KERNEL_VERSION}:kernel_aarch64_abi_dist -- --dist_dir=${KERNEL_ROOT_DIR}/${KERNEL_BAZEL_DIST_OUT}/abi
